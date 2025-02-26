5c8b929ef83cbecde77d0060ffd73d48dde4d6ac
Akash Singh <akashsingh16423@gmail.com>
2025-02-26 11:17:34 +0530

    Update

--- src/main/java/com/hibernate/cfg/HibernateConfiguration.java
+++ src/main/java/com/hibernate/cfg/HibernateConfiguration.java
@@ -16,7 +16,7 @@ public class HibernateConfiguration {
 		Properties ps = new Properties();
 
 		ps.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
-		ps.put(Environment.URL, "jdbc:mysql://localhost:3306/piyush");
+		ps.put(Environment.URL, "jdbc:mysql://localhost:3306/mydb");
 		ps.put(Environment.USER, "root");
 		ps.put(Environment.PASS, "root");
 		ps.put(Environment.HBM2DDL_AUTO, "create");
